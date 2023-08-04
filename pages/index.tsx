76955881ddb8b142c2b8e1a444940ba715e31796
Hugo Zhan <hugozhan0802@gmail.com>
2023-08-03 17:55:27 -0700

    add logs

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ export default function IndexPage() {
 
   const onCreateLinkClick = async (e: any) => {
     const result = await createLink(e.target.id);
+    console.log(result);
     router.push("info/" + result?.data._id);
   };
 
